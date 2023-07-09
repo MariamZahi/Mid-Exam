7a0226a82db671aa69791b187560c48386c3c076
Mariam Zahid <120362767+MariamZahi@users.noreply.github.com>
2023-07-09 17:22:09 -0400

    Import spinner

---
+++ app/src/main/java/com/example/unitconvert/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.unitconvert
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
